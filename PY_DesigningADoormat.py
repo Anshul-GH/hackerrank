106366c2d4c71e1e0865a4a54a2283279cb74c0a
Anshul <samgalacestyle01@gmail.com>
2020-02-15 01:23:07 +0530

    Home Commit - JypyterTipsTricks and Dormat problem.

---
+++ PY_DesigningADoormat.py
@@ -0,0 +1,13 @@
+'''
+Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications:
+
+    Mat size must be 
+
+X. ( is an odd natural number, and is times
+.)
+The design should have 'WELCOME' written in the center.
+The design pattern should only use |, . and - characters.
+
+https://www.hackerrank.com/challenges/designer-door-mat/problem?h_r=next-challenge&h_v=zen
+'''
+
