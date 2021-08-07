78415460e710c5f75755e1453a94fbe09b720212
AJ <samgalacestyle01@gmail.com>
2021-08-08 00:36:06 +0530

    Routine Commit

--- Solutions/53. 2DArrayPrintingPatterns.py
+++ Solutions/53. 2DArrayPrintingPatterns.py
@@ -46,45 +46,65 @@ https://www.hackerrank.com/challenges/alphabet-rangoli/problem
 '''
 
 
-def print_rangoli(size):
-    # # ASCII starter for alphabets
-    # start = 97
-    # chrct = chr(start + size - 1)
-    # # print(chrct)
+# def print_rangoli(size):
+#     # # ASCII starter for alphabets
+#     # start = 97
+#     # chrct = chr(start + size - 1)
+#     # # print(chrct)
 
-    # length = 4*size - 3
+#     # length = 4*size - 3
     
-    # for i in range(2*size - 1):        
-    #     for j in range(length):            
-    #         if j == 2*size/2+1:
-    #             print(chr(start + size - 1 - i), end='')
-    #         else:
-    #             print('-', end='')
-    #     print()
+#     # for i in range(2*size - 1):        
+#     #     for j in range(length):            
+#     #         if j == 2*size/2+1:
+#     #             print(chr(start + size - 1 - i), end='')
+#     #         else:
+#     #             print('-', end='')
+#     #     print()
 
-    width = 4*size - 3
-    start = 97
-    chrct = chr(start + size - 1)
+#     width = 4*size - 3
+#     start = 97
+#     chrct = chr(start + size - 1)
       
-    # Print the pattern having a top triangle 
-    for i in range (0, int (size - 1)): 
-        pattern = ('-' + chr(start + size - 1 - i) + '-') * ((2 * i) + 1) 
-        print (pattern.center (width, '-')) 
+#     # Print the pattern having a top triangle 
+#     for i in range (0, int (size - 1)): 
+#         pattern = ('-' + chr(start + size - 1 - i) + '-') * ((2 * i) + 1) 
+#         print (pattern.center (width, '-')) 
       
-    # # Print GeeksforGeeks in the center 
-    # print ("GeeksforGeeks".center (width, '-')) 
+#     # # Print GeeksforGeeks in the center 
+#     # print ("GeeksforGeeks".center (width, '-')) 
       
-    # Print the pattern having  
-    # an inverted triangle 
-    i = int (size - 1) 
-    while i > 0: 
-        pattern = ('-' + chr(start + size - 1 - i) + '-') * ((2 * i) - 1) 
-        print (pattern.center (width, '-')) 
-        i = i-1
-    return
+#     # Print the pattern having  
+#     # an inverted triangle 
+#     i = int (size - 1) 
+#     while i > 0: 
+#         pattern = ('-' + chr(start + size - 1 - i) + '-') * ((2 * i) - 1) 
+#         print (pattern.center (width, '-')) 
+#         i = i-1
+#     return
 
 
 
-if __name__ == '__main__':
-    n = int(input())
-    print_rangoli(n)
+# if __name__ == '__main__':
+#     n = int(input())
+#     print_rangoli(n)
+
+
+def print_rangoli(n):
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+    data = [alpha[i] for i in range(n)]
+    print(data)
+    
+    items = list(range(n))
+    print(items)
+
+    print(items[:-1], items[::-1])
+
+    items = items[:-1]+items[::-1]
+    for i in items:
+        temp = data[-(i+1):]
+        row = temp[::-1]+temp[1:]
+        print("-".join(row).center(n*4-3, "-"))
+
+n = int(input())
+print_rangoli(n)
